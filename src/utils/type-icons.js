b9cb6e19cfca4725de50919b694ba05b94a77bc9
Veidz <joaovictorveidz@gmail.com>
2022-09-05 20:33:05 -0300

    feat: add all icons to a single file

---
+++ src/utils/type-icons.js
@@ -0,0 +1,58 @@
+import defaultIcon from '../assets/default.png'
+import barber from '../assets/barber.png'
+import basket from '../assets/basket.png'
+import bike from '../assets/bike.png'
+import birthday from '../assets/birthday.png'
+import bowling from '../assets/bowling.png'
+import call from '../assets/call.png'
+import car from '../assets/car.png'
+import climb from '../assets/climb.png'
+import food from '../assets/food.png'
+import games from '../assets/games.png'
+import gym from '../assets/gym.png'
+import hospital from '../assets/hospital.png'
+import job from '../assets/job.png'
+import mail from '../assets/mail.png'
+import money from '../assets/money.png'
+import motorcycle from '../assets/motorcycle.png'
+import movie from '../assets/movie.png'
+import people from '../assets/people.png'
+import phone from '../assets/phone.png'
+import shop from '../assets/shop.png'
+import soccer from '../assets/soccer.png'
+import tennis from '../assets/tennis.png'
+import trip from '../assets/trip.png'
+import vet from '../assets/vet.png'
+import volley from '../assets/volley.png'
+
+const typeIcons = [
+  null,
+  defaultIcon,
+  barber,
+  basket,
+  bike,
+  birthday,
+  bowling,
+  call,
+  car,
+  climb,
+  food,
+  games,
+  gym,
+  hospital,
+  job,
+  mail,
+  money,
+  motorcycle,
+  movie,
+  people,
+  phone,
+  shop,
+  soccer,
+  tennis,
+  trip,
+  vet,
+  volley
+]
+
+export default typeIcons
