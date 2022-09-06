3cc84af658a147bf8bbe40f4befa76ccd2faa570
Veidz <joaovictorveidz@gmail.com>
2022-09-06 18:51:15 -0300

    feat: create add task implementation to add to database

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -10,6 +10,13 @@ import icons from '../../utils/type-icons'
 function Task () {
   const [lateCount, setLateCount] = useState()
   const [type, setType] = useState(1)
+  const [id, setId] = useState()
+  const [done, setDone] = useState(false)
+  const [title, setTitle] = useState()
+  const [description, setDescription] = useState()
+  const [date, setDate] = useState()
+  const [hour, setHour] = useState()
+  const [macaddress, setMacaddress] = useState('11:11:11:11:11:11')
 
   useEffect(() => {
     lateVerify()
@@ -18,6 +25,20 @@ function Task () {
   async function lateVerify () {
     const lateTasks = await api.get('/tasks/filter/late/11:11:11:11:11:11')
     setLateCount(lateTasks.data.length)
+  }
+
+  async function save () {
+    try {
+      await api.post('/tasks', {
+        macaddress,
+        type,
+        title,
+        description,
+        when: `${date}T${hour}:00.000`
+      })
+    } catch (error) {
+      console.log(error.message)
+    }
   }
 
   return (
@@ -38,34 +59,34 @@ function Task () {
 
         <InputContainer>
           <h3>Title</h3>
-          <input type='text' placeholder='Task title...' />
+          <input type='text' placeholder='Task title...' onChange={ (e) => setTitle(e.target.value) } value={ title } />
         </InputContainer>
 
         <TextAreaContainer>
           <h3>Description</h3>
-          <textarea rows={ 5 } placeholder='Task details...' />
+          <textarea rows={ 5 } placeholder='Task details...' onChange={ (e) => setDescription(e.target.value) } value={ description } />
         </TextAreaContainer>
 
         <InputContainer>
           <h3>Date</h3>
-          <input type='date' placeholder='Task date...' />
+          <input type='date' placeholder='Task date...' onChange={ (e) => setDate(e.target.value) } value={ date } />
         </InputContainer>
 
         <InputContainer>
           <h3>Hour</h3>
-          <input type='time' placeholder='Task hour...' />
+          <input type='time' placeholder='Task hour...' onChange={ (e) => setHour(e.target.value) } value={ hour } />
         </InputContainer>
 
         <OptionsContainer>
           <div>
-            <input type='checkbox' />
+            <input type='checkbox' checked={ done } onChange={ () => setDone(!done) } />
             <span>COMPLETED</span>
           </div>
           <button type='button'>DELETE</button>
         </OptionsContainer>
 
         <AddContainer>
-          <button type='button'>ADD</button>
+          <button type='button' onClick={ save }>ADD</button>
         </AddContainer>
       </FormContainer>
 
